e4c25b80861ac563ff19d1660e67621e3b50d97c
Madhusudan Jha <madhusudanjha14071999@gmail.com>
2022-03-06 18:38:16 +0530

    Routing practiced

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { FirstComponent } from './first/first.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'first-component', component: FirstComponent, children:  [
+    {path: 'child-a', component: ChildAComponent},
+    {path: 'child-b', component: ChildBComponent, children: [
+      {path: 'crisis-list', component: CrisisListComponent},
+    ]},  
+  ]},
+  {path: 'second-component',  component: SecondComponent, children: [
+    {path:'heroes-list', component: HeroesListComponent},
+  ]},
+
+  {path:'', redirectTo: '/first-component', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ChildAComponent,
+    ChildBComponent,
+    CrisisListComponent,
+    HeroesListComponent
   ],
   imports: [
     BrowserModule,
